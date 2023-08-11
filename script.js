19a65171a3f5988f8bc49cf753d34f4a734074d2
Aurwa Zafar <zafaraurwa11@gmail.com>
2023-08-11 12:58:12 +0500

    add to local storage

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ const todoList = document.querySelector(".todo-list");
 
 //event-listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //functions
+
+//function addTodo
 function addTodo(event) {
   event.preventDefault();
   const todoDiv = document.createElement("div");
-  //todoDiv.classList.add('mx-auto', 'p-4', 'rounded-md', 'shadow-2xl');
-
   todoDiv.classList.add(
     "todo",
-    "bg-purple-200",
+    "bg-purple-400",
     "pl-3",
     "rounded-lg",
     "flex",
@@ -24,9 +25,13 @@ function addTodo(event) {
   const newTodo = document.createElement("li");
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item", "flex-1");
+  todoDiv.append(newTodo);
+  //local storage
+  saveLocalTodos(todoInput.value);
   //complete button
   const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.innerHTML =
+    '<i class="fas fa-check pointer-events-none"></i>';
   completedButton.classList.add(
     "complete-btn",
     "bg-green-600",
@@ -37,7 +42,7 @@ function addTodo(event) {
   );
   //delete button
   const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.innerHTML = '<i class="fas fa-trash pointer-events-none"></i>';
   trashButton.classList.add(
     "trash-btn",
     "bg-red-600",
@@ -47,7 +52,34 @@ function addTodo(event) {
     "px-3",
     "rounded-r-lg"
   );
-  todoDiv.append(newTodo, completedButton, trashButton);
+  todoDiv.append(completedButton, trashButton);
   todoList.appendChild(todoDiv);
   todoInput.value = "";
+}
+
+//function deleteCheck
+function deleteCheck(event) {
+  const item = event.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("line-through", "opacity-40");
+  }
+}
+
+//function saveLocalTodos
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
